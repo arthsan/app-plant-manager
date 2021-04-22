7329ea8541c11ea1f9543124dae0942b1ac18902
arthsan <arthur_sbd@hotmail.com>
2021-04-22 03:17:22 -0300

    second day

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,35 +1,47 @@
-import React from 'react';
-import {TouchableOpacity, StyleSheet, Text } from 'react-native'
-import colors from '../styles/colors'
+import React from "react";
+import {
+  TouchableOpacity,
+  TouchableOpacityProps,
+  StyleSheet,
+  Text,
+} from "react-native";
+import { Feather } from "@expo/vector-icons";
+import colors from "../styles/colors";
+import fonts from "../styles/fonts";
 
-interface ButtonProps {
+interface ButtonProps extends TouchableOpacityProps {
   title: string;
+  nextTo: Boolean;
 }
 
-export function Button({ title }: ButtonProps){
+export function Button({ title, nextTo, ...rest }: ButtonProps) {
   return (
-    <TouchableOpacity style={styles.button} activeOpacity={0.7}>
-    <Text style={styles.buttonText}>
-      {title}
-    </Text>
-  </TouchableOpacity>
-  )
+    <TouchableOpacity style={styles.container} activeOpacity={0.7} {...rest}>
+      <Text style={styles.buttonText}>
+        {title}
+        {nextTo && <Feather name="chevron-right" style={styles.buttonIcon} />}
+      </Text>
+    </TouchableOpacity>
+  );
 }
 
 const styles = StyleSheet.create({
-  button: {
+  container: {
     backgroundColor: colors.green,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     borderRadius: 16,
     marginBottom: 10,
     height: 56,
-    paddingHorizontal: 10
+    paddingHorizontal: 10,
   },
   buttonText: {
     color: colors.white,
-    fontSize: 24
-  }
-}
-)
-
+    fontFamily: fonts.heading,
+    fontSize: 24,
+  },
+  buttonIcon: {
+    color: colors.white,
+    fontSize: 24,
+  },
+});
